f0ed30bdb14396e191f60994c5427bf22111fd8b
Danny Smooke <dana.smooke@gmail.com>
2020-11-21 21:18:55 -0500

    tried to make input of username save to localStorage....it's a shit show, need help

--- savegame.js
+++ savegame.js
@@ -1,10 +1,14 @@
-const mostRecentScore = document.querySelector('#mostRecentScore')
-const highScores = JSON.parse(localStorage.getItem('highScores')) || []
-const maxHighScores = 5
+var mostRecentScore = document.querySelector('#finalScore')
+var highScores = JSON.parse(localStorage.getItem('#finalScore')) || []
+const maxHighScores = 500;
 
 
-finalScore.innerText = mostRecentScore
+finalScore.innerText = maxHighScores;
 
 username.addEventListener('keyup', () => {
-    saveScoreBtn.disabled = !username.value
+    saveBtn.disabled = !username.value
 });
+
+let numCorrect = mostRecentScore;
+localStorage.setItem(highScores, JSON.stringify(numCorrect));
+let pastScores = localStorage.get(JSON.parse(highScores))
